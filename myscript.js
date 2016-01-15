b1cbfaa9a821f09d605443597f0bf09c7262c807
msherman1229 <msherman1229@gmail.com>
2016-01-15 20:01:44 +0000

    First commit

--- myscript.js
+++ myscript.js
@@ -1,4 +1,7 @@
 function motivate(str)
-{
-	console.log(str);
+{ 
+	document.getElementById("name-head").innerHTML = str;
+	document.getElementById("name-head").style.color = "red"; 
+	var names = str.split(" "); 
+	document.getElementById("name-head").innerHTML += names[0] + " is your first name" ; 
 }
