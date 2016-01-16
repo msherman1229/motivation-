c8f4f003fe96bafd4e8c6af8043ae3854726d927
msherman1229 <msherman1229@gmail.com>
2016-01-16 19:04:54 +0000

    First commit

--- myscript.js
+++ myscript.js
@@ -1,7 +1,18 @@
 function motivate(str)
 { 
-	document.getElementById("name-head").innerHTML = str;
-	document.getElementById("name-head").style.color = "red"; 
-	var names = str.split(" "); 
-	document.getElementById("name-head").innerHTML += names[0] + " is your first name" ; 
+	document.getElementById("name-head").innerHTML = str; 
+	var names = str.split(" ");
+	var firstName = names[0]; 
+	document.getElementById("name-head").innerHTML += names[0] + " is your first name" ;
+	var option1 = firstName + ", if you hear a voice within you say \"you cannot paint,\" then by all means paint, and that voice will be silenced."; 
+	var option2 = "Dear " + firstName + "Success consists of going from failure to failure without loss of enthusiasm. " + "Love, Winston Churchill"; 
+	var option3 = firstName + " Your time is limited, don't waste it living someone else's life. Don't be trapped by dogma, which is living the result of other people's thinking. Don't let the noise of other's opinion drowned your own inner voice. And most important, " + firstName + " have the courage to follow your heart and intuition, they somehow already know what you truly want to become. Everything else is secondary."; 
+	var option4 = firstName + ", When one door closes, another opens; but we often look so long and so regretfully upon the closed door that we do not see the one which has opened for us. Wishing you the best, Alexander Graham Bell";
+	var option5 = firstName + " our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time. I hope you choose to make today a good day " + firstName + ", Sincerely Thomas Edison"; 
+	var options = [option1, option2, option3, option4, option5];
+	function getRandomInt(min, max) {
+	return Math.floor(Math.random() * (max-min)) + min;
+	}
+	console.log(getRandomInt(0,options.length));
+	document.getElementById("m-paragraph").innerHTML = options[getRandomInt(0,options.length)];   
 }
