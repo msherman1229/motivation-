3825ca41012fa10193387be1f4a3840ae65c5c4f
msherman1229 <msherman1229@gmail.com>
2016-01-18 19:57:06 +0000

    First commit

--- myscript.js
+++ myscript.js
@@ -4,11 +4,11 @@ function motivate(str)
 	var names = str.split(" ");
 	var firstName = names[0]; 
 	var option1 = "<b>" + firstName + "<b>,<br>If you hear a voice within you say \"you cannot paint,\" then by all means paint, and that voice will be silenced.<br>Your friend and constant supporter,<br>Vincent Van Gogh"; 
-	var option2 = "Dear " + firstName + ",<br>" + "Success consists of going from failure to failure without loss of enthusiasm.<br>" + "Love,<br>Winston Churchill"; 
-	var option3 = firstName + ",<br> Your time is limited, don't waste it living someone else's life. Don't be trapped by dogma, which is living the result of other people's thinking. Don't let the noise of other's opinion drowned your own inner voice. And most important, " + firstName + ", have the courage to follow your heart and intuition, they somehow already know what you truly want to become. Everything else is secondary.<br>Keep your head up " + firstName + ",<br>Steve Jobs"; 
-	var option4 = firstName + ",<br>When one door closes, another opens; but we often look so long and so regretfully upon the closed door that we do not see the one which has opened for us.<br>Wishing you the best,<br>Alexander Graham Bell";
-	var option5 = firstName + ",<br>Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.<br>I hope you choose to try today " + firstName + ",<br>Thomas Edison"; 
-	var option6 = "Just so you know " + firstName + ",<br>To love oneself is the beginning of a lifelong romance.<br>Your friend and confident,<br>Oscar Wilde";
+	var option2 = "Dear <b>" + firstName + "</b>,<br>" + "Success consists of going from failure to failure without loss of enthusiasm.<br>" + "Love,<br>Winston Churchill"; 
+	var option3 = "<b>" + firstName + "</b>,<br> Your time is limited, don't waste it living someone else's life. Don't be trapped by dogma, which is living the result of other people's thinking. Don't let the noise of other's opinion drowned your own inner voice. And most important, <b>" + firstName + "</b>, have the courage to follow your heart and intuition, they somehow already know what you truly want to become. Everything else is secondary.<br>Keep your head up <b>" + firstName + "</b>,<br>Steve Jobs"; 
+	var option4 = "<b>" + firstName + "</b>,<br>When one door closes, another opens; but we often look so long and so regretfully upon the closed door that we do not see the one which has opened for us.<br>Wishing you the best,<br>Alexander Graham Bell";
+	var option5 = "<b>" + firstName + "</b>,<br>Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.<br>I hope you choose to try today <b>" + firstName + "</b>,<br>Thomas Edison"; 
+	var option6 = "Just so you know <b>" + firstName + "</b>,<br>To love oneself is the beginning of a lifelong romance.<br>Your friend and confident,<br>Oscar Wilde";
 	var options = [option1, option2, option3, option4, option5, option6];
 	function getRandomInt(min, max) {
 	return Math.floor(Math.random() * (max-min)) + min;
